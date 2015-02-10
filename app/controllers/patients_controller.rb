efaf812cbae227c66a1dfd756999388662a2a3f4
Paul Carter <p-carter@kellogg.northwestern.edu>
2015-02-10 17:12:24 -0600

    Update

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -8,9 +8,14 @@ class PatientsController < ApplicationController
     @patient = Patient.new
   end
 
+  def show
+    @patient = Patient.find_by(id: params["id"])
+    @esas_assesments = EsasAssesment.where(patient_id: @patient.id)
+  end
+
   def create
-  	patient_params = params.require(:patient).permit(:first_name)
-  	Patient.create(patient_params)
+  	patient_params = params.require(:patient).permit(:first_name,:last_name,:username,:diagnosis,:gender,:age,:address, :email, :password, :phone_number, :caregiver_name, :other_sypmtom, :goals_of_care, :patient_deceased, :patient_archived)
+    Patient.create(patient_params)
     #render text: params["patient"]
     redirect_to patients_path
   end
@@ -20,7 +25,7 @@ class PatientsController < ApplicationController
 	end
 
 	def update
-    patient_params = params.require(:patient).permit(:first_name)
+    patient_params = params.require(:patient).permit(:first_name,:last_name,:username,:diagnosis,:gender,:age,:address, :email, :password, :phone_number, :caregiver_name, :other_sypmtom, :goals_of_care, :patient_deceased, :patient_archived)
     @patient = Patient.find_by(id: params["id"])
     @patient.update_attributes(patient_params)
     redirect_to patients_path
