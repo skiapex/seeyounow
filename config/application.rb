e4cbae4bf8805f77c1807c3580be0f598177c5bc
Paul Carter <p-carter@kellogg.northwestern.edu>
2015-03-09 10:43:04 -0500

    going backwards

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module SeeYouNow
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,4 +21,5 @@ module SeeYouNow
     # config.i18n.default_locale = :de
   end
 
+  config.assets.initialize_on_precompile = false
 end
