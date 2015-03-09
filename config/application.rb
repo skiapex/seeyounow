af4b8b434be4a98c1c3e7d9b847c1231e56f2f30
Paul Carter <p-carter@kellogg.northwestern.edu>
2015-03-09 09:46:20 -0500

    Trying to get rid of precompile fail

--- config/application.rb
+++ config/application.rb
@@ -20,4 +20,6 @@ module SeeYouNow
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+
+  config.assets.initialize_on_precompile = false
 end
