573fc12d01c2d271e69f564c1cafae6a5efe4161
Paul Carter MAC <p-carter@kellogg.northwestern.edu>
2015-04-13 20:09:42 -0700

    Trying to Raygun to work

--- config/initializers/raygun.rb
+++ config/initializers/raygun.rb
@@ -2,6 +2,8 @@ Raygun.setup do |config|
   config.api_key = "njEC/WfFrqQlenCugGPU9g=="
   config.filter_parameters = Rails.application.config.filter_parameters
 
+  config.enable_reporting = true
+
   # The default is Rails.env.production?
   # config.enable_reporting = !Rails.env.development? && !Rails.env.test?
 end
