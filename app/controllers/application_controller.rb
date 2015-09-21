f5f9ad384886255c9bd36da4efa0760fa661ffae
Paul Carter MAC <p-carter@kellogg.northwestern.edu>
2015-09-21 08:45:34 -0700

    find_by_auth_token! to find_by!(auth_token:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   before_action :require_admin
 
   def current_user
-    @current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+    @current_user ||= User.find_by!(auth_token: cookies[:auth_token]) if cookies[:auth_token]
   end
 
   def require_user
